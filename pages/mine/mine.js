ae2ab361abdd4511d012a064345d227632aa7422
pcd <changdapan@gmail.com>
2021-05-19 02:33:03 +0800

    minePage updated

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,9 +5,77 @@ Page({
    * 页面的初始数据
    */
   data: {
-    content:["2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0","2018-10-24 v1.0.0",]
+    pastList: [{
+      id: '0',
+      date: '2021-5-7 - 2021-5-11',
+      name: '重庆之旅',
+      picList: [],
+    }, {
+      id: '1',
+      date: '2021-5-7 - 2021-5-11',
+      name: '重庆之旅',
+      picList: [],
+    }, {
+      id: '2',
+      date: '2021-5-7 - 2021-5-11',
+      name: '重庆之旅',
+      picList: [],
+    }, ],
+    futureList: [{
+      id: '10',
+      date: '2021-6-8 - 2021-6-11',
+      name: '上海之旅',
+      picList: [],
+    }, {
+      id: '11',
+      date: '2021-6-8 - 2021-6-11',
+      name: '上海之旅',
+      picList: [],
+    }, {
+      id: '12',
+      date: '2021-6-8 - 2021-6-11',
+      name: '上海之旅',
+      picList: [],
+    }, {
+      id: '13',
+      date: '2021-6-8 - 2021-6-11',
+      name: '上海之旅',
+      picList: [],
+    }, ],
+    actions: [{
+        name: "行程详情",
+      },
+      {
+        name: "删除行程",
+      },
+      {
+        name: "分享行程",
+      }
+    ],
+    show: false,
+    pageNo: 0,
+    pageSize: 5,
+    loading: true,
   },
-
+  onClick(e) {
+    console.log(e)
+    this.setData({
+      show: true,
+    })
+  },
+  onClose() {
+    this.setData({
+      show: false,
+    })
+  },
+  onCancel() {
+    this.setData({
+      show: false,
+    })
+  },
+  onSelect(e) {
+    console.log(e.detail)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -54,7 +122,15 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    let pageNo = this.data.pageNo + 1;
+    let content = this.data.content;
+    for (let i = 0; i < this.data.pageSize; i++)
+      content.push("2021/5/7 - 2021/5/8 重庆之旅");
+    this.setData({
+      content,
+      pageNo,
+    })
+    console.log('@@onReachBottom triggered', pageNo)
   },
 
   /**
